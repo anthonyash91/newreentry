050eb4b4ce27340447a7cb2d4db2aaff22ba0597
Anthony Ashcraft <anthonyashcraft91@gmail.com>
2025-01-31 01:00:09 -0500

    deploying

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+import path from "path";
 const coursesRoutes = require("./routes/courses");
 const categoriesRoutes = require("./routes/categories");
 
@@ -16,18 +17,21 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
 // routes
 app.use("/api/courses", coursesRoutes);
 app.use("/api/categories", categoriesRoutes);
 
-// connect to db
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`listening on port ${process.env.PORT}`);
+if (produc)
+  // connect to db
+  mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+      app.listen(process.env.PORT, () => {
+        console.log(`listening on port ${process.env.PORT}`);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
     });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
